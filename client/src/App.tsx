96154a26599f21c14c7f12c52e61595287cd14b5
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2023-12-01 12:54:05 +0100

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react'
+
+export function App(): ReactElement {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="my-2 text-xl text-center">CorAs Analyzer</h1>
+      </div>
+    </>
+  )
+}
